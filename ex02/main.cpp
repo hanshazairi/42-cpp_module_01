5abe8c95f3778b5f988c90ff0e9e5943fd8e0b9d
Hans Hazairi <hanshazairi@gmail.com>
2022-02-02 00:14:53 +0800

    chore: finish ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbaddrul <hbaddrul@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/02 00:00:39 by hbaddrul          #+#    #+#             */
+/*   Updated: 2022/02/02 00:13:49 by hbaddrul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+int main(void) {
+    string  str = "HI THIS IS BRAIN";
+    string  *stringPTR = &str;
+    string  &stringREF = str;
+
+    cout << &str << endl;
+    cout << stringPTR << endl;
+    cout << &stringREF << endl;
+
+    cout << str << endl;
+    cout << *stringPTR << endl;
+    cout << stringREF << endl;
+
+    return 0;
+}
